d49f6fc0a44a91ab5b89b3f4e1a98fac45d6d813
Aoi-hosizora <a970335605@hotmail.com>
2024-05-13 13:03:40 +0800

    refa: split storage api related functions from storage.ts

--- src/ts/data/storage.ts
+++ src/ts/data/storage.ts
@@ -1,4 +1,5 @@
 import { URLInfo } from '@src/ts/data/model';
+import { getStorage, removeStorage, setStorage } from '@src/ts/data/api';
 
 // ==============
 // global related
@@ -57,44 +58,6 @@ export async function readStorageToGlobal(): Promise<void> {
     Global.showUserPrivateCounter = await getStorage<boolean>(StorageFlag.SHOW_USER_PRIVATE_COUNTER, true, { alsoInit: true });
     Global.showRepoActionCounter = await getStorage<boolean>(StorageFlag.SHOW_REPO_ACTION_COUNTER, true, { alsoInit: true });
     Global.showRepoAndContentsSize = await getStorage<boolean>(StorageFlag.SHOW_REPO_AND_CONTENTS_SIZE, true, { alsoInit: true });
-}
-
-// ===============
-// storage related
-// ===============
-
-const STORAGE = chrome.storage.sync || chrome.storage.local;
-
-export async function setStorage(flag: StorageFlag, value: string | number | boolean): Promise<void> {
-    return new Promise((resolve, _) => {
-        const obj = { [flag.toString()]: value };
-        STORAGE.set(obj, () => resolve());
-    });
-}
-
-export async function getStorage<T extends string | number | boolean>(flag: StorageFlag, defaultValue: T, etc: { alsoInit?: boolean } = {}): Promise<T> {
-    return new Promise((resolve, _) => {
-        STORAGE.get(flag.toString(), items => {
-            var value = items[flag.toString()];
-            if (value === undefined || value === null || typeof value !== typeof defaultValue) {
-                if (etc.alsoInit) {
-                    const obj = { [flag.toString()]: defaultValue };
-                    STORAGE.set(obj, () => resolve(defaultValue));
-                } else {
-                    resolve(defaultValue);
-                }
-            } else {
-                resolve(value as T);
-            }
-        });
-    });
-
-}
-
-export async function removeStorage(flag: StorageFlag): Promise<void> {
-    return new Promise((resolve, _) => {
-        STORAGE.remove(flag.toString(), () => resolve());
-    });
 }
 
 // =============
